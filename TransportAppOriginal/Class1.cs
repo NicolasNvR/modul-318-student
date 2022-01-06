60e260277324a69375b5566d4d8e6d52f3014c9c
nicolasvonrotz <nicolas.vonrotz@bbv.ch>
2022-01-06 13:14:16 +0100

    Änderung auf .Net 6.0 Neues Pojekt - TansportAppOriginal

---
+++ TransportAppOriginal/Class1.cs
@@ -0,0 +1,17 @@
+﻿using SwissTransport.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportAppOriginal
+{
+    internal class Class1
+    {
+        public void test()
+        {
+            ITransport transport = new Transport();
+        }
+    }
+}
